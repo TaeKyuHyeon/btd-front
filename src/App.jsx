b051ab9ae19eda92ae740d42e091ea3b0380831a
ara_ko <and_ara@nts-corp.com>
2024-01-10 15:23:24 +0900

    [#1] 🐶 husky test

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
                         Edit <code>src/App.js</code> and save to reload.
                     </p>
                     <a className={cx('App-link')} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                        Learn React test
+                        Learn React
                     </a>
                 </header>
             </div>
