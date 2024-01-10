6a7aa9cae49a6ccd73bd5caae4911084706f764d
ara_ko <and_ara@nts-corp.com>
2024-01-10 14:27:51 +0900

    [#1] 🔧  prettier 설정 변경 / classnames 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
+import classNames from 'classnames/bind';
 import logo from './logo.svg';
-import './App.scss';
+import styles from './App.module.scss';
+
+const cx = classNames.bind(styles);
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+  return (
+    <div className={cx('App')}>
+      <header className={cx('App-header')}>
+        <img src={logo} className={cx('App-logo')} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className={cx('App-link')} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
