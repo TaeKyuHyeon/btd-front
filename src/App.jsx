04e3feb3d631aaedfecfd4bb48dba3afae3e8a31
ara_ko <and_ara@nts-corp.com>
2024-01-10 15:20:08 +0900

    [#1] 🔧  githooks husky 설정

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
                         Edit <code>src/App.js</code> and save to reload.
                     </p>
                     <a className={cx('App-link')} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                        Learn React
+                        Learn React test
                     </a>
                 </header>
             </div>
