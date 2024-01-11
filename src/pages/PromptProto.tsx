b6a7aad9fb35d68b2874760cb713307822a24a5b
arako <kar9299@gmail.com>
2024-01-11 18:48:07 +0900

    [#7] 🌱 로딩중 컴포넌트 추가

--- src/pages/PromptProto.tsx
+++ src/pages/PromptProto.tsx
@@ -1,24 +1,31 @@
 import {useState} from 'react';
 
 import ButtonDraw from '@/components/button/ButtonDraw';
+import Loading from '@/components/common/Loading';
 import Aside from '@/components/layout/Aside';
 import Panel from '@/components/layout/Panel';
 import LineNote from '@/components/LineNote';
 
 function PromptProto() {
     const [value, setValue] = useState<string>('');
+    const [isLoading] = useState<boolean>(false);
 
+    // 서버에 이미지 생성 요청
+    const callDraw = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log(e);
+    };
+
+    // 패널에 컨텐츠 렌더링
+    const renderContent = () => {
+        if (isLoading) return <Loading text="그림 그리는 중..." />;
+
+        return <LineNote value={value} setValue={setValue} placeholder="명령어를 입력하세요." />;
+    };
     return (
         <>
-            <Panel>
-                <LineNote value={value} setValue={setValue} placeholder="명령어를 입력하세요." />
-            </Panel>
+            <Panel>{renderContent()}</Panel>
             <Aside>
-                <ButtonDraw
-                    onClick={(e) => {
-                        console.log(e);
-                    }}
-                />
+                <ButtonDraw onClick={callDraw} />
             </Aside>
         </>
     );
