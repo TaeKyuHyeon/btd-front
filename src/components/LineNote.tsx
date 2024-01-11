09b4cbcd7d4e6509e4fba67bea0340752e9366ba
arako <kar9299@gmail.com>
2024-01-11 17:37:17 +0900

    [#3] 🌱 라인 노트 추가

--- src/components/LineNote.tsx
+++ src/components/LineNote.tsx
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 import classNames from 'classnames/bind';
 
 import styles from './LineNote.module.scss';
@@ -5,7 +7,19 @@ import styles from './LineNote.module.scss';
 const cx = classNames.bind(styles);
 
 function NotePad() {
-    return <div className={cx('article')} />;
+    const [value, setValue] = useState<string>('');
+
+    return (
+        <div className={cx('article')}>
+            <textarea
+                className={cx('textarea')}
+                value={value}
+                onChange={(e) => {
+                    setValue(e.target.value);
+                }}
+            />
+        </div>
+    );
 }
 
 export default NotePad;
