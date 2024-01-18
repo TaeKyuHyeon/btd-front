dc952700fec59fd107595aefe56193d3091c416e
arako <kar9299@gmail.com>
2024-01-18 15:45:16 +0900

    [#13] # Please enter the commit message for your changes. Lines starting

--- src/components/PictureDiary.tsx
+++ src/components/PictureDiary.tsx
@@ -1,8 +1,13 @@
 // import classNames from 'classnames/bind';
 
+import {indexArray} from '@/utils/common';
+
 // import styles from './PictureDiary.module.scss';
 
 // const cx = classNames.bind(styles);
+
+const ROW_COUNT = 5;
+const COLUMN_COUNT = 10;
 
 function PictureDiary() {
     return (
@@ -11,10 +16,10 @@ function PictureDiary() {
                 <div className="info_area">
                     <div className="date">
                         <input type="date" />
-                        <div className="year">년</div>
-                        <div className="month">월</div>
-                        <div className="date">일</div>
-                        <div className="day">요일</div>
+                        <span className="year">년</span>
+                        <span className="month">월</span>
+                        <span className="date">일</span>
+                        <span className="day">요일</span>
                     </div>
                     <div className="weather">
                         <input type="radio" id="sun" name="weather" />
@@ -25,8 +30,17 @@ function PictureDiary() {
                 </div>
                 <div className="image_area">
                     <input type="file" />
+                    <img src="" alt="" />
                 </div>
-                div.
+                <div className="text_area">
+                    {indexArray(ROW_COUNT).map((row) => (
+                        <div className="row" key={row}>
+                            {indexArray(COLUMN_COUNT).map((col) => (
+                                <div className="cell" key={col} />
+                            ))}
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
