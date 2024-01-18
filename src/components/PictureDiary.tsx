15c68030c332241a30521ee152e5e8458d2c3bce
arako <kar9299@gmail.com>
2024-01-19 04:18:19 +0900

    [#13] 💄 반응형 레이아웃 고정 크기로 수정

--- src/components/PictureDiary.tsx
+++ src/components/PictureDiary.tsx
@@ -6,17 +6,24 @@ import styles from './PictureDiary.module.scss';
 
 const cx = classNames.bind(styles);
 
-const MAX_LENGTH = 60;
+const MAX_LENGTH = 50;
 const INITIAL_ARRAY: Array<{index: number; value: string | null}> = Array.from({length: MAX_LENGTH}, (v, i) => ({
     index: i,
     value: null,
 }));
+const today = new Date();
+const DAY_KOR = ['일', '월', '화', '수', '목', '금', '토'];
 
 function PictureDiary() {
     const [textValue, setTextValue] = useState('');
     const [textArray, setTextArray] = useState(INITIAL_ARRAY);
     const [selection, setSelection] = useState<{start: number | null; end: number | null}>();
     const textInput = useRef<HTMLInputElement>(null);
+
+    const [dateValue, setDateValue] = useState<Date>(today);
+    const dateInput = useRef<HTMLInputElement>(null);
+
+    // const dateToObj = () => {};
 
     const changeSelection = (e: React.KeyboardEvent<HTMLInputElement>) => {
         const {selectionStart, selectionEnd} = e.target as HTMLInputElement;
@@ -45,14 +52,45 @@ function PictureDiary() {
         <div className={cx('article')}>
             <div className={cx('box')}>
                 <div className={cx('info_area')}>
-                    <div className={cx('date')}>
-                        <input type="date" />
-                        <span className={cx('year')}>년</span>
-                        <span className={cx('month')}>월</span>
-                        <span className={cx('date')}>일</span>
-                        <span className={cx('day')}>요일</span>
+                    <div className={cx('info_item', 'date')}>
+                        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/label-has-associated-control */}
+                        <label
+                            className={cx('label')}
+                            onClick={() => {
+                                dateInput.current?.showPicker();
+                            }}>
+                            <input
+                                type="date"
+                                className={(cx('input'), 'blind')}
+                                ref={dateInput}
+                                onChange={(e) => {
+                                    const {value} = e.target;
+                                    const arr = value.split('-');
+                                    const newDate = new Date(Number(arr[0]), Number(arr[1]) - 1, Number(arr[2]));
+                                    setDateValue(newDate);
+                                }}
+                            />
+                        </label>
+                        <div className={cx('text_area')}>
+                            <span className={cx('text', 'year')}>
+                                <span className={cx('num')}>{dateValue.getFullYear()}</span>
+                                <span className={cx('unit')}>년</span>
+                            </span>
+                            <span className={cx('text', 'month')}>
+                                <span className={cx('num')}>{dateValue.getMonth()}</span>
+                                <span className={cx('unit')}>월</span>
+                            </span>
+                            <span className={cx('text', 'date')}>
+                                <span className={cx('num')}>{dateValue.getDate() + 1}</span>
+                                <span className={cx('unit')}>일</span>
+                            </span>
+                            <span className={cx('text', 'day')}>
+                                <span className={cx('num')}>{DAY_KOR[dateValue.getDay()]}</span>
+                                <span className={cx('unit')}>요일</span>
+                            </span>
+                        </div>
                     </div>
-                    <div className={cx('weather')}>
+                    <div className={cx('info_item', 'weather')}>
                         <input type="radio" id="sun" name="weather" />
                         <input type="radio" id="cloud" name="weather" />
                         <input type="radio" id="rain" name="weather" />
--- src/components/layout/Panel.tsx
+++ src/components/layout/Panel.tsx
@@ -6,35 +6,24 @@ import styles from './Panel.module.scss';
 
 const cx = classNames.bind(styles);
 
+const HOLE_COUNT = 6;
+
 function Panel({children}: PropsWithChildren) {
     return (
         <div className={cx('article')}>
-            <div className={cx('LineNote')}>
-                <div className={cx('hole_area')}>
-                    <div className={cx('hole')}>
-                        <div className={cx('ring')} />
-                    </div>
-                    <div className={cx('hole')}>
-                        <div className={cx('ring')} />
-                    </div>
-                    <div className={cx('hole')}>
-                        <div className={cx('ring')} />
-                    </div>
-                    <div className={cx('hole')}>
-                        <div className={cx('ring')} />
-                    </div>
-                    <div className={cx('hole')}>
-                        <div className={cx('ring')} />
-                    </div>
-                    <div className={cx('hole')}>
-                        <div className={cx('ring')} />
-                    </div>
-                    <div className={cx('hole')}>
-                        <div className={cx('ring')} />
-                    </div>
+            <div className={cx('note_area')}>
+                <div className={cx('page', 'left')}>
+                    <div className={cx('content_area')}>{children}</div>
                 </div>
-                <div className={cx('content_area')}>{children}</div>
+                <div className={cx('page', 'right')}>
+                    <div className={cx('content_area')}>{children}</div>
+                </div>
             </div>
+            <ul className={cx('hole_area')}>
+                {Array.from({length: HOLE_COUNT}, (v, i) => i + 1).map((key) => (
+                    <li className={cx('hole')} key={key} />
+                ))}
+            </ul>
         </div>
     );
 }
